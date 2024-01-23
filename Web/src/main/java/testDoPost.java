026156e1f48852a09744065e18ea82ab1523f87d
Nguyễn Đức Thắng <120944583+Mrk4tsu@users.noreply.github.com>
2024-01-23 10:04:15 +0700

    doPost

---
+++ Web/src/main/java/testDoPost.java
@@ -0,0 +1,50 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/TestDoPost")
+public class testDoPost extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.append("Bạn vừa yêu cầu Get");
+        String noidungHtml = "<form method=\"post\" action=\"TestDoPost\">" +
+                "<label>Họ</label>" +
+                "<input name=\"fname\" type=\"text\">" +
+                "<br>" +
+                "<label>Tên</label>" +
+                "<input name=\"lname\" type=\"text\">" +
+                "<br>" +
+                "<input type=\"submit\" value=\"Gửi\">" +
+                "</form>";
+        writer.append(noidungHtml);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        String value1 = req.getParameter("fname");
+        String value2 = req.getParameter("lname");
+
+        PrintWriter writer = resp.getWriter();
+
+        writer.append("Bạn vừa gửi yêu cầu POST");
+        writer.append("<br>");
+        writer.append("Giá trị tham số fname: ");
+        writer.append(value1);
+        writer.append("<br>");
+
+        writer.append("Giá trị tham số lname: ");
+        writer.append(value2);
+    }
+}
--- Web/src/main/webapp/index.jsp
+++ Web/src/main/webapp/index.jsp
@@ -2,5 +2,15 @@
 <body>
 <h2>Hello World!Welcome to my rule</h2>
 <a href="/Web/HelloWorld">Start Servlet</a>
+
+<form method="post" action="TestDoPost">
+    <label>Họ</label>
+    <input name="fname" type="text">
+    <br>
+    <label>Tên</label>
+    <input name="lname" type="text">
+    <br>
+    <input type="submit" value="Gửi">
+</form>
 </body>
 </html>
