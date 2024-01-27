b46c166ce09db299adbdd280b532b5eaf6971b69
Nguyễn Đức Thắng <120944583+Mrk4tsu@users.noreply.github.com>
2024-01-27 10:51:19 +0700

    Add dependency to connect MySQL, create Models Class

---
+++ MyWebApp/src/main/java/com/mrkatsu/mywebapp/models/User.java
@@ -0,0 +1,70 @@
+package com.mrkatsu.mywebapp.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String firstname;
+    @Column(nullable = false, unique = true, length = 45)
+    private String lastname;
+    @Column(nullable = false, unique = true, length = 45)
+    private String avatar;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
