d3fd3fcd681e9ee9c62fd7776fc4badd43635177
Nguyễn Đức Thắng <113938996+zdtkazg@users.noreply.github.com>
2024-01-28 07:39:35 +0700

    Code Update User

--- MyWebApp/src/main/java/com/mrkatsu/mywebapp/controllers/UserController.java
+++ MyWebApp/src/main/java/com/mrkatsu/mywebapp/controllers/UserController.java
@@ -2,10 +2,13 @@ package com.mrkatsu.mywebapp.controllers;
 
 import com.mrkatsu.mywebapp.models.User;
 import com.mrkatsu.mywebapp.services.UserService;
+import com.mrkatsu.mywebapp.utilities.UserNotFoundException;
+import com.mrkatsu.mywebapp.utilities.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +27,7 @@ public class UserController {
     @GetMapping("/users/new")
     public String showUserForm(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Create New User");
         return "user_form";
     }
     @PostMapping("/users/save")
@@ -31,6 +35,20 @@ public class UserController {
             service.save(user);
 
             attributes.addFlashAttribute("message", "The user has been created successfully.");
-            return "redirect:/users";
+            return Utils.Redirect("users");
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String showFormEdit(@PathVariable("id") Integer id, Model model, RedirectAttributes attributes){
+        try {
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (" + id + ")");
+
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            attributes.addFlashAttribute("message", "The user has been created successfully.");
+            return Utils.Redirect("users");
+        }
     }
 }
