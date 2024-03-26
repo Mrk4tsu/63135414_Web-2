e069b3bac3a5f77e7efe27774324c0d701111bab
Nguyễn Đức Thắng <120944583+Mrk4tsu@users.noreply.github.com>
2024-03-26 15:25:20 +0700

    Push lần 1

--- IntelliJ/NguyenDucThang_QuanLyUser/src/main/java/thiGK/ntu63135414/controllers/MainController.java
+++ IntelliJ/NguyenDucThang_QuanLyUser/src/main/java/thiGK/ntu63135414/controllers/MainController.java
@@ -1,4 +1,12 @@
 package thiGK.ntu63135414.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
