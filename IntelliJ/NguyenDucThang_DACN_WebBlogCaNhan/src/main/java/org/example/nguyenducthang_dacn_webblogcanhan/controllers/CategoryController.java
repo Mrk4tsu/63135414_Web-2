5334b9298ba6904c39ec6326c48ce206590ca0df
Nguyễn Đức Thắng <120944583+Mrk4tsu@users.noreply.github.com>
2024-05-21 02:46:31 +0700

    Lấy và in danh sách danh mục

--- IntelliJ/NguyenDucThang_DACN_WebBlogCaNhan/src/main/java/org/example/nguyenducthang_dacn_webblogcanhan/controllers/CategoryController.java
+++ IntelliJ/NguyenDucThang_DACN_WebBlogCaNhan/src/main/java/org/example/nguyenducthang_dacn_webblogcanhan/controllers/CategoryController.java
@@ -1,10 +1,25 @@
 package org.example.nguyenducthang_dacn_webblogcanhan.controllers;
 
+import org.example.nguyenducthang_dacn_webblogcanhan.models.Category;
 import org.example.nguyenducthang_dacn_webblogcanhan.services.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
     private CategoryService service;
 
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/danh-muc")
+    public String showCategoryPage(Model model) {
+        List<Category> listCategory = service.listAll();
+        model.addAttribute("listCategory", listCategory);
+        return "category/category";
+    }
 }
