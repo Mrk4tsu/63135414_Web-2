38ca5692339ed13f95f64627a028495f85065921
Nguyễn Đức Thắng <120944583+Mrk4tsu@users.noreply.github.com>
2024-05-20 22:52:32 +0700

    Chia thư mục giao diện, thêm sửa thông tin người dùng

---
+++ IntelliJ/NguyenDucThang_DACN_WebBlogCaNhan/src/main/java/org/example/nguyenducthang_dacn_webblogcanhan/utils/Utilities.java
@@ -0,0 +1,7 @@
+package org.example.nguyenducthang_dacn_webblogcanhan.utils;
+
+public class Utilities {
+    public static String Redirect(String redirect){
+        return "redirect:/" + redirect;
+    }
+}
